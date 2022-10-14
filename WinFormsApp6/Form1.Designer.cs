cfedbc6bc9d55e8e177999502bfd8d1eb2cdaf2a
Ananolij <Ananolij366@outlook.com>
2022-10-14 19:57:47 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp6/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace WinFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.sizeTB = new System.Windows.Forms.TextBox();
+            this.outputButton = new System.Windows.Forms.Button();
+            this.outputTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(18, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "n =";
+            // 
+            // sizeTB
+            // 
+            this.sizeTB.Location = new System.Drawing.Point(53, 10);
+            this.sizeTB.Name = "sizeTB";
+            this.sizeTB.Size = new System.Drawing.Size(115, 23);
+            this.sizeTB.TabIndex = 1;
+            // 
+            // outputButton
+            // 
+            this.outputButton.Location = new System.Drawing.Point(53, 39);
+            this.outputButton.Name = "outputButton";
+            this.outputButton.Size = new System.Drawing.Size(115, 35);
+            this.outputButton.TabIndex = 7;
+            this.outputButton.Text = "Вывод";
+            this.outputButton.UseVisualStyleBackColor = true;
+            this.outputButton.Click += new System.EventHandler(this.outputButton_Click);
+            // 
+            // outputTB
+            // 
+            this.outputTB.Location = new System.Drawing.Point(203, 12);
+            this.outputTB.Multiline = true;
+            this.outputTB.Name = "outputTB";
+            this.outputTB.ReadOnly = true;
+            this.outputTB.Size = new System.Drawing.Size(399, 377);
+            this.outputTB.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputTB);
+            this.Controls.Add(this.outputButton);
+            this.Controls.Add(this.sizeTB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sizeTB;
+        private System.Windows.Forms.Button outputButton;
+        private System.Windows.Forms.TextBox outputTB;
+    }
+}
+
---
+++ WinFormsApp6/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void outputButton_Click(object sender, EventArgs e)
+        {
+            outputTB.Text = "";
+            int size = Int32.Parse(sizeTB.Text);
+            int[,] matrix = new int[size, size];
+            Random random = new Random();
+            for(int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = random.Next(0, 100);
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                int[] array = getOneArray(matrix, i);
+                sort(array);
+                insertRow(array, matrix, i);
+
+                for (int j = 0; j < size; j++)
+                {
+                    outputTB.Text += matrix[i, j] + "\t";
+                }
+                outputTB.Text += "\r\n";
+            }
+        }
+
+        private int[] getOneArray(int[,] array, int row)
+        {
+            int[] result = new int[array.GetLength(1)];
+            for(int i = 0; i < array.GetLength(1); i++)
+            {
+                result[i] = array[row, i];
+            }
+            return result;
+        }
+
+        public void insertRow(int[] array, int[,] matrix, int row)
+        {
+            for(int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[row, i] = array[i];
+            }
+        }
+
+        private void sort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int k = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > k)
+                {
+                    array[j + 1] = array[j];
+                    array[j] = k;
+                    j--;
+                }
+            }
+            //return array;
+        }
+    }
+}
